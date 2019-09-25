9d093098fb69402f9b1d4cc800c7cfc93369889e
unknown <2120367@talnet.nl>
2019-09-25 12:35:03 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,19 @@
+<?php
+
+$userAge;
+$minimumAge;
+
+echo "Hoe oud ben je? " . "\n";
+
+$userAge = readline();
+
+if ($userAge <= 16.4)
+{
+  echo "Je mag nog geen auto rijden";
+}
+else
+{
+  echo "Je mag autorijden";
+}
+
+ ?>
